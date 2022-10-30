12fedfd73faeae12386b94737f432c321e0f84ea
Shanice Jones <shanicejones567890@gmail.com>
2022-10-30 03:21:34 -0500

    All files

---
+++ scripts.js
@@ -0,0 +1,16 @@
+onload= function(){
+    var email= document.getElementById("email");
+    var message= document.getElementsByClassName('message')[0];
+    
+    this.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        if(email.value !=''){
+            message.innerHTML= `Thank you! your email address ${email.value} has been added to our mailing list` ;
+            email.value='';
+        }else{
+            message.innerHTML='Please enter a valid email address';
+            email.value='';
+        }
+        
+    })
+}
